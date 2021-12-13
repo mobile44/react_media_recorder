10c1feb849708d84d234ca60efacad2504a6922e
mobile44 <albertwong.wk@gmail.com>
2021-12-12 17:00:38 -0800

    add: update App.js router third time

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
         <div className="divBreak"/>
       </div>
       <Routes>
-        <Route path="/react_media_recorder" element={<Home pageName="Home"/>}/>
+        <Route path="/home" element={<Home pageName="Home"/>}/>
         <Route path="/about" element={<About pageName="About"/>} />
         <Route path="/video" element={<Video pageName="Video"/>} />
         <Route path="/audio" element={<Audio pageName="Audio"/>} />
